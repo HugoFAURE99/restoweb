28454487acc734c506b3c9c32e599c7dfe6de644
HugoFAURE99 <hugo.faure@limayrac.fr>
2024-09-26 14:48:38 +0200

    added payer.php and confirmation.php (still need some CSS adjustment)

---
+++ src/features/confirmation/confirmation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../app.css">
+    <link rel="stylesheet" href="confirmation.css">
+    <title>Resto Web</title>
+</head>
+<body>
+    <?php
+    include "../../components/navBar.php";
+    ?>
+    <div class="page">
+        <h2>Commande confirmée!</h2>
+        <p>Merci de votre commande!</p>
+        <a href="../../index.php">Retour à la page d'accueil</a>
+    </div>
+</body>
+</html>
