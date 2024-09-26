4057e08d8615f23e2963c92665f9af8470f292b6
HugoFAURE99 <hugo.faure@limayrac.fr>
2024-09-26 14:40:23 +0200

    patched order in navBar button

--- src/components/navBar.php
+++ src/components/navBar.php
@@ -5,8 +5,8 @@ echo "<link rel='stylesheet' href='/projets/RestoWeb/restoweb/src/components/nav
 $navBarElements = array(
     array("label" => "Accueil", "link" => "/projets/RestoWeb/restoweb/src/index.php"),
     array("label" => "Connexion", "link" => "/projets/RestoWeb/restoweb/src/features/connection/connection.php"),
-    array("label" => "Deconnexion", "link" => "/projets/RestoWeb/restoweb/src/index.php" ),
     array("label" => "Inscription", "link" => "/projets/RestoWeb/restoweb/src/features/inscription/inscription.php"),
+    array("label" => "Deconnexion", "link" => "/projets/RestoWeb/restoweb/src/index.php" ),
     //Add array here that contains the navigation bar buttons info to add a button to the navigation bar 
 );
 
