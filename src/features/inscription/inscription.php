2a64ebd81fe43f07399a42072e6e8eeff118e3d7
Mbarki Nouh <nouhmbarki31@gmail.com>
2024-09-27 13:20:08 +0200

    liste des taches a faire

---
+++ src/features/inscription/inscription.php
@@ -0,0 +1,4 @@
+1) formulaire inspiré de connection.php
+2) a la fin quand tu appuye sur submit, ca redirige vers la page connexion.php
+3) avec le lien du fichier connection.php dans le action 
+3) le css garder le meme, coier coller et le mettre dans "inscritpion.css"
