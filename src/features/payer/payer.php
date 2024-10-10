2e9e36b7b7533f3a816c408feeb93078a49aa6e0
George Khitaridze <george.khitaridze@limayrac.fr>
2024-10-10 11:56:41 +0200

    added before_ligne_update.sql + sql in payer.php

--- src/features/payer/payer.php
+++ src/features/payer/payer.php
@@ -1,3 +1,35 @@
+<?php
+include "../../functions/connect_db.php";
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header("Location: ../connection/connection.php");
+    exit;
+}
+
+$pdo = connect_db(); 
+$error_message = null;
+$submit = isset($_POST['submit']);
+
+if ($submit) {
+    $idCom = $_POST['idCom'];
+
+    try {
+        $etatCom = "Calculée";
+        $sql = "UPDATE Commande SET etatCom = :etatCom WHERE idCom = :idCom";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ':etatCom' => $etatCom,
+            ':idCom' => $idCom
+        ]);
+
+        echo "L'état de la commande avec id $idCom a été mis à jour avec succès.";
+    } catch (PDOException $error) {
+        echo "Erreur lors de la mise à jour de la commande : " . $error->getMessage();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,22 +41,21 @@
     <title>Resto Web</title>
 </head>
 <body>
-    <?php
-    include "../../components/navBar/navBar.php";
-    ?>
-
+    <?php include "../../components/navBar/navBar.php"; ?>
 
     <div class="page">
         <div class="payment_container">
             <h1>Paiement de votre commande</h1>
-            <form action="../confirmation/confirmation.php" method="post">
-                <input type="text" name="cb_number" placeholder="N° de CB">
-                <input type="text" name="exp_date" placeholder="Date expiration">
-                <input type="text" name="ccv" placeholder="CCV">
-                <input type="submit" value="Payer">
+            <form action="" method="post">
+   
+                <input type="hidden" name="idCom" value="4">
+                
+                <input type="text" name="cb_number" placeholder="N° de CB" required="required">
+                <input type="text" name="exp_date" placeholder="Date expiration" required="required">
+                <input type="text" name="ccv" placeholder="CCV" required="required">
+                <input type="submit" name ="submit" value="Payer">
             </form>
         </div>
     </div>
-    
 </body>
 </html>
